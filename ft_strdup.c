f89aa504d1a4c2ae2f5a994f249f6667aa471792
Totolosa <thomas.dayde@gmail.com>
2020-12-08 21:32:38 +0100

    Modif strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,38 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tdayde <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: tdayde <tdayde@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 16:56:03 by tdayde            #+#    #+#             */
-/*   Updated: 2020/11/26 14:11:50 by tdayde           ###   ########lyon.fr   */
+/*   Updated: 2020/12/08 21:31:55 by tdayde           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_strcpy(char *dest, const char *src)
+char	*ft_strdup(const char *s)
 {
-	int i;
+	char	*dest;
+	int		i;
 
+	if ((dest = malloc(sizeof(char) * gnl_strlen(s) + 1)) == NULL)
+		return (NULL);
 	i = 0;
-	while (src[i])
+	while (s[i])
 	{
-		dest[i] = src[i];
+		dest[i] = s[i];
 		i++;
 	}
 	dest[i] = '\0';
-	return (dest);
-}
-
-char		*ft_strdup(const char *s)
-{
-	char *dest;
-
-	dest = malloc(sizeof(char) * ft_strlen(s) + 1);
-	if (dest == NULL)
-	{
-		return (NULL);
-	}
-	dest = ft_strcpy(dest, s);
 	return (dest);
 }
